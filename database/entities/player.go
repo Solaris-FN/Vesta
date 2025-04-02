b3a10dd0ae65f792f2b5b3cbde527d0a122ef2d9
Itztiva <tiva@Itztiva.com>
2025-04-02 18:13:57 -0400

    oops!

--- database/entities/player.go
+++ database/entities/player.go
@@ -1,13 +1,15 @@
-package entities 
+package entities
 
 import (
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Player struct {
 	gorm.Model
-	AccountID        string         `gorm:"column:account_id;primaryKey"`
-	Session        string         `gorm:"column:session"`
+	AccountID string         `gorm:"column:account_id;primaryKey"`
+	Session   string         `gorm:"column:session"`
+	Team      pq.StringArray `gorm:"column:team;type:text[]"`
 }
 
 func (Player) TableName() string {
